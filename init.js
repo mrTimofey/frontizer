6551399860d4cc505ce7c86e827e00fd13eaf3b2
Timofey Tiunov <timoxa17@gmail.com>
2015-10-08 12:29:51 +0300

    init.js reworked

--- init.js
+++ init.js
@@ -3,33 +3,48 @@
  */
 
 var fs = require('fs'),
-	file,
 	// default config
 	config = {
 		appPort: 3000,
 		livereloadPort: 35729
-	};
+	},
+	// files to create on initialization
+	files = [
+		{
+			name: 'assets/js/main.js',
+		},
+		{
+			name: 'assets/styles/main.styl',
+			content: '@import kouto-swiss\n\nnormalize()'
+		},
+		{
+			name: 'views/layout.jade',
+			content: function() {
+				return "doctype html\n" +
+				"html\n\t" +
+					"head\n\t\t" +
+						"meta(charset='UTF-8')\n\t\t" +
+						"title=title\n\t\t" +
+						"link(href=__css rel='stylesheet')\n\t" +
+					"body\n\t\t" +
+						"main\n\t\t\t" +
+						"block main\n\t\t" +
+						"script(src=__js)" +
+				(config.livereloadPort ? "\n\t\tscript(src=__livereload)" : '');
+			}
+		},
+		{
+			name: 'views/home.jade',
+			content: 'extends layout\nblock main\n\th1 Home page'
+		},
+		{
+			name: 'data/home.js',
+			content: "exports.title = 'Hello, world!'"
+		}
+	];
 
-try {
-	file = fs.openSync('assets/js/main.js', 'wx');
-	console.log('assets/js/main.js created');
-}
-catch(e) {
-	if (e.code === 'EEXIST') console.log('assets/js/main.js exists');
-	else throw e;
-}
-
-try {
-	file = fs.openSync('assets/styles/main.styl', 'wx');
-	fs.writeSync(file, "@import kouto-swiss\n\nnormalize()");
-	console.log('assets/styles/main.styl created');
-}
-catch(e) {
-	if (e.code === 'EEXIST') console.log('assets/styles/main.styl exists');
-	else throw e;
-}
-
-file = fs.openSync('config.json', 'w');
+// parse config
+var file = fs.openSync('config.json', 'w');
 process.argv.slice(2).forEach(function (v) {
 	v = v.split('=');
 	v[0] = v[0].replace('--', '').trim();
@@ -39,36 +54,15 @@ process.argv.slice(2).forEach(function (v) {
 fs.writeSync(file, JSON.stringify(config));
 console.log('config.json created');
 
-try {
-	file = fs.openSync('views/layout.jade', 'wx');
-	fs.writeSync(file,
-		"doctype html\nhtml\n\thead\n\t\tmeta(charset='UTF-8')\n\t\ttitle=title\n\t\tlink(href=__css rel='stylesheet')" +
-		"\n\tbody\n\t\tmain\n\t\t\tblock main\n\t\tscript(src=__js)" +
-			(config.livereloadPort ? "\n\t\tscript(src=__livereload)" : '')
-	);
-	console.log('views/layout.jade created');
-}
-catch(e) {
-	if (e.code === 'EEXIST') console.log('views/layout.jade exists');
-	else throw e;
-}
-
-try {
-	file = fs.openSync('views/home.jade', 'wx');
-	fs.writeSync(file, "extends layout\nblock main\n\th1 Home page");
-	console.log('views/home.jade created');
-}
-catch(e) {
-	if (e.code === 'EEXIST') console.log('views/home.jade exists');
-	else throw e;
-}
-
-try {
-	file = fs.openSync('data/home.js', 'wx');
-	fs.writeSync(file, "exports.title = 'Hello, world!'");
-	console.log('data/home.js created');
-}
-catch(e) {
-	if (e.code === 'EEXIST') console.log('data/home.js exists');
-	else throw e;
-}
+files.forEach(function(file) {
+	var f;
+	try {
+		f = fs.openSync(file.name, 'wx');
+		if (file.content) fs.writeSync(f, typeof file.content === 'function' ? file.content() : file.content);
+		console.log(file.name + ' created');
+	}
+	catch(e) {
+		if (e.code === 'EEXIST') console.log(file.name + ' already exists');
+		else throw e;
+	}
+});
