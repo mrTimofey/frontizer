edec54e0cbde58682616ef1165f29979369e5098
Timofey <timoxa17@gmail.com>
2016-05-31 13:02:24 +0300

    app server is optional now

--- commands/start.js
+++ commands/start.js
@@ -15,76 +15,78 @@ module.exports = function(options, home) {
 	options = options || {};
 	home = home || process.cwd();
 
-	var app = express(),
-		config = require(home + '/config.json'),
+	var config = require(home + '/config.json'),
 		browserifyArgs = config.browserify || [],
 		stylusArgs = config.stylus || [];
 
 	locals.init('app', home, config);
 
 	// APP SERVER
+	if (config.appPort) {
+		var app = express();
 
-	app.set('views', home + '/views');
-	app.set('view engine', 'jade');
-	app.set('view cache', false);
+		app.set('views', home + '/views');
+		app.set('view engine', 'jade');
+		app.set('view cache', false);
 
-	// statics
-	let faviconFile = fs.existsSync(home + '/favicon.ico') ?
-		(home + '/favicon.ico') : (__dirname + '/../favicon.ico');
+		// statics
+		let faviconFile = fs.existsSync(home + '/favicon.ico') ?
+			(home + '/favicon.ico') : (__dirname + '/../favicon.ico');
 
-	app.use(favicon(faviconFile));
-	app.use('/assets', express.static(home + '/assets'));
+		app.use(favicon(faviconFile));
+		app.use('/assets', express.static(home + '/assets'));
 
-	// populate helpers
-	Object.keys(locals).forEach(k => app.locals[k] = locals[k]);
+		// populate helpers
+		Object.keys(locals).forEach(k => app.locals[k] = locals[k]);
 
-	// allows absolute path in 'extends' for jade
-	app.locals.basedir = app.get('views');
+		// allows absolute path in 'extends' for jade
+		app.locals.basedir = app.get('views');
 
-	// indented html output
-	if (options.pretty) app.locals.pretty = '\t';
+		// indented html output
+		if (options.pretty) app.locals.pretty = '\t';
 
-	// serve api
-	app.use(/^\/api\/(.*)$/, (req, res) => {
-		var src = home + '/api/' + req.params[0];
-		fs.exists(src + '.js', exists => {
-			if (!exists) return res.status(404).end('API not found: ' + src);
-			try {
-				let api = require(src);
-				delete require.cache[require.resolve(src)];
-				if (typeof api === 'function') return api(req, res);
-				else return res.json(api);
-			}
-			catch (e) {
-				res.status(500).end(e.toString());
-			}
+		// serve api
+		app.use(/^\/api\/(.*)$/, (req, res) => {
+			var src = home + '/api/' + req.params[0];
+			fs.exists(src + '.js', exists => {
+				if (!exists) return res.status(404).end('API not found: ' + src);
+				try {
+					let api = require(src);
+					delete require.cache[require.resolve(src)];
+					if (typeof api === 'function') return api(req, res);
+					else return res.json(api);
+				}
+				catch (e) {
+					res.status(500).end(e.toString());
+				}
+			});
 		});
-	});
-	
-	// serve views
-	app.get(/^\/(.*)$/, (req, res) => {
-		var reqPath = req.params[0];
-		if (reqPath === '') reqPath = 'home';
 
-		helpers.lookupData(reqPath, (data) => {
-			if (config.livereloadPort)
-				data.__livereload ='<script src="//' +
-					req.hostname + ':' + config.livereloadPort + '/livereload.js"></script>';
-			else data.__livereload = false;
-			data.req = req;
-			try {
-				res.render(reqPath, data, (err, output) => {
-					if (err) res.status(500).end(err.toString());
-					else res.end(output);
-				});
-			}
-			catch (e) {
-				res.status(404).end('Not found');
-			}
+		// serve views
+		app.get(/^\/(.*)$/, (req, res) => {
+			var reqPath = req.params[0];
+			if (reqPath === '') reqPath = 'home';
+
+			helpers.lookupData(reqPath, (data) => {
+				if (config.livereloadPort)
+					data.__livereload ='<script src="//' +
+						req.hostname + ':' + config.livereloadPort + '/livereload.js"></script>';
+				else data.__livereload = false;
+				data.req = req;
+				try {
+					res.render(reqPath, data, (err, output) => {
+						if (err) res.status(500).end(err.toString());
+						else res.end(output);
+					});
+				}
+				catch (e) {
+					res.status(404).end('Not found');
+				}
+			});
 		});
-	});
 
-	app.listen(config.appPort);
+		app.listen(config.appPort);
+	}
 
 	// LIVERELOAD SERVER
 
